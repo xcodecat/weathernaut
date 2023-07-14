416d4bf3b6da3d2b53f535fead2bacf221606e74
xcodecat <xmissh0lyc4t@gmail.com>
2023-07-14 20:22:21 +0200

    feat: added all parameters

--- main.py
+++ main.py
@@ -12,17 +12,35 @@ app = Flask(__name__)
 def index(weather_data=[]):
     if request.method == 'POST':
         city = request.form.get('city')
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.environ["OPENWEATHERMAP_API_KEY"]}&units' \
-              f'=metric'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.environ["OPENWEATHERMAP_API_KEY"]}&units=metric'
         response = requests.get(url)
         data = dict(response.json())
-        weather = dict(city=data['name'], main=data['weather'][0]['main'], temperature=data['main']['temp'],
-                       max=data['main']['temp_max'], min=data['main']['temp_min'], feels=data['main']['feels_like'])
+        print(data)
+        weather = dict(city=data['name'],
+                       main=data['weather'][0]['main'],
+                       description=data['weather'][0]['description'],
+                       temperature=data['main']['temp'],
+                       max=data['main']['temp_max'],
+                       min=data['main']['temp_min'],
+                       feels=data['main']['feels_like'],
+                       humidity=data['main']['humidity'],
+                       wind_speed=data['wind']['speed'],
+                       pressure=data['main']['pressure'],
+                       sunrise=data['sys']['sunrise'],
+                       sunset=data['sys']['sunset'],
+                       timezone=data['timezone'])
         weather_data.append(weather)
         return render_template('weather.html', city=city, main=data['weather'][0]['main'],
+                               description=data['weather'][0]['description'],
                                temperature=data['main']['temp'],
                                max=data['main']['temp_max'], min=data['main']['temp_min'],
-                               feels=data['main']['feels_like'])
+                               feels=data['main']['feels_like'],
+                               humidity=data['main']['humidity'],
+                               wind_speed=data['wind']['speed'],
+                               pressure=data['main']['pressure'],
+                               sunrise=data['sys']['sunrise'],
+                               sunset=data['sys']['sunset'],
+                               timezone=data['timezone'])
     return render_template('index.html', weather_data=weather_data)
 
 
